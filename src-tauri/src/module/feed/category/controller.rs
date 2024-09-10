acc6f097af95f0e3a220f9199562d86525bb4a6b
StevenDesroches <stevendesroches@hotmail.com>
2024-09-09 22:16:58 -0400

    FEAT: Get all categories in controller

--- src-tauri/src/module/feed/category/controller.rs
+++ src-tauri/src/module/feed/category/controller.rs
@@ -12,11 +12,31 @@ impl CategoryController {
 
     pub async fn add(&self, title: String, parent_id: Option<i32>) -> Result<()> {
         let category = match parent_id {
-            Some(parent_id) =>  Category::builder().title(title).parent(parent_id).build(),
+            Some(parent_id) => Category::builder().title(title).parent(parent_id).build(),
             None => Category::builder().title(title).build(),
         };
-        CategoryModel::new().open().insert_category(category)?.close()?;
+        CategoryModel::new()
+            .open()
+            .insert_category(category)?
+            .close()?;
         Ok(())
     }
 
+    pub fn get_all(&self) -> Result<Vec<Category>> {
+        let model = CategoryModel::new().open();
+        let mut categories = Vec::new();
+
+        let model_categories = model.get_categories()?;
+        model.close()?;
+        let _: Vec<_> = model_categories
+            .into_iter()
+            .map(|i| match i.1 {
+                Some(parent_id) => {
+                    categories.push(Category::builder().id(i.0).parent(parent_id).title(i.2).build())
+                }
+                None => categories.push(Category::builder().id(i.0).title(i.2).build()),
+            })
+            .collect();
+        Ok(categories)
+    }
 }
