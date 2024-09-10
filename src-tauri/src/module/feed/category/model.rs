187678ca970c0f9af2f4156792b0a47f4cf8b0d1
StevenDesroches <stevendesroches@hotmail.com>
2024-09-09 22:16:07 -0400

    FEAT: GET categories from modal and adding better error

--- src-tauri/src/module/feed/category/model.rs
+++ src-tauri/src/module/feed/category/model.rs
@@ -26,15 +26,43 @@ impl CategoryModel {
             .db
             .connection
             .as_ref()
-            .ok_or(Error::Custom("DB NOT OPEN".to_string()))?;
+            .ok_or(Error::Model("DB NOT OPEN".to_string()))?;
 
         connection
             .execute(
                 "INSERT INTO category (title, parent_id) VALUES (?1, ?2)",
                 (category.title, category.parent_id),
             )
-            .map_err(|e| Error::Custom(e.to_string()))?;
+            .map_err(|e| Error::Model(e.to_string()))?;
         // let category_id = connection.last_insert_rowid();
         Ok(self)
+    }
+
+    pub fn get_categories(&self) -> Result<Vec<(i32, Option<i32>, String)>> {
+        let connection = self
+            .db
+            .connection
+            .as_ref()
+            .ok_or(Error::Model("DB NOT OPEN".to_string()))?;
+
+        let mut statement = connection
+            .prepare("SELECT * FROM category;")
+            .map_err(|e| Error::Model(e.to_string()))?;
+
+        let rows = statement
+            .query_map([], |row| {
+                Ok((
+                    row.get::<_, i32>(0)?,
+                    row.get::<_, Option<i32>>(1)?,
+                    row.get::<_, String>(2)?,
+                ))
+            })
+            .map_err(|e| Error::Model(e.to_string()))?;
+
+        let mut categories: Vec<(i32, Option<i32>, String)> = Vec::new();
+        for row in rows {
+            categories.push(row.map_err(|e| Error::Model(e.to_string()))?);
+        }
+        Ok(categories)
     }
 }
